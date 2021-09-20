0ca5a2174cddc28d4ae007bac08f98e856dad533
Pedro Collin <67438145+PedroCollin@users.noreply.github.com>
2021-09-20 10:13:13 -0300

    comitin

---
+++ src/components/Cardinfo.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="cardinfo" :style="cssCustom">
+        <section class="sectionInfo">
+            <div class="card-body">
+                <h5>{{text_title}}</h5>
+                <p>{{text_content}}</p>
+            </div>
+        </section>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Cardinfo",
+        props: ["text_title", "text_content", "back_color", "text_color", "imgs"],
+        computed: {
+            cssCustom() {
+                return {
+                    "--mycolor": this.text_color,
+                    "--myback": this.back_color,
+                };
+            }
+        }
+    }
+</script>
+
+<style scoped src="./Cardinfo.css">
+
+</style>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="home">
-    <h1>Bem vindo ao <br> Roberto Manshi </h1>
-  </div> 
+  <div>
+    <div class="home">
+      <h1>Bem vindo ao <br> Roberto Manshi </h1>
+    </div> 
+      <Cardinfo 
+        text_title:="Tratamento especial"
+        text_content="Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado."
+        back_color="black"
+        text_color="white"
+      />
+  </div>
 </template>
 
 <script>
 
+import Cardinfo from '../components/Cardinfo.vue'
+
 export default {
     name: 'Home',
+    components: {
+      Cardinfo
+    } 
 }
 </script>
 
